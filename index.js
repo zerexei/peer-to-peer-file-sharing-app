6ce4a12d443f36cae754fad34c3a8a19ad08e30a
Angelo Arcillas <angeloarcillas64@gmail.com>
2025-03-08 09:19:03 +0800

    wip: peer connection

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-const peer = new Peer();
-console.log(peer);
+let peerConnection = null;
+
+const elements = {
+  peerStartSession: document.querySelector("button.peer-start-session"),
+  peerId: document.querySelector(".peer-id"),
+};
+
+class PeerConnection {
+  session = null;
+  connections = new Map();
+
+  constructor() {
+    this.session = new Peer();
+
+    peer.on("open", function (id) {
+      this.connections.set(id, true);
+      this.setPeerId(id);
+    });
+  }
+
+  setPeerId(id) {
+    elements.peerId.innerText = `ID: ${id}`;
+  }
+}
+
+elements.peerStartSession.addEventListener("click", () => {
+  peerConnection = new PeerConnection();
+});
